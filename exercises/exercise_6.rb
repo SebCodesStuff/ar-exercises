874e299b81ccf305d90c781313dca1e8b9c27f87
vagrant <seb.namek@gmail.com>
2017-11-07 22:26:40 +0000

    ex6 done

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,31 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create([
+  {first_name: "Khurram",
+  last_name: "Virani",
+  hourly_rate: 60},
+  {first_name: "Adam",
+  last_name: "Brown",
+  hourly_rate: 20},
+  {first_name: "Jordan",
+  last_name: "Zak",
+  hourly_rate: 60},
+  {first_name: "Steph",
+  last_name: "Dallaire",
+  hourly_rate: 90},
+])
+@store2.employees.create([
+  {first_name: "Seb",
+  last_name: "Namek",
+  hourly_rate: 70},
+  {first_name: "Jon",
+  last_name: "Orsi",
+  hourly_rate: 50},
+  {first_name: "Katie",
+  last_name: "Namek",
+  hourly_rate: 55},
+  {first_name: "Gill",
+  last_name: "McLellan",
+  hourly_rate: 100}
+  ])
