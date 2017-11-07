6a052da02598328585f285e20590593c1577d0c9
vagrant <seb.namek@gmail.com>
2017-11-07 22:02:16 +0000

    ex4 done

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,38 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+stores = Store.create([
+  {name: 'Burnaby',
+  annual_revenue: '300000',
+  mens_apparel: true,
+  womens_apparel: true},
+  {name: 'Richmond',
+  annual_revenue: '1260000',
+  mens_apparel: false,
+  womens_apparel: true},
+  {name: 'Gastown',
+  annual_revenue: '190000',
+  mens_apparel: true,
+  womens_apparel: false},
+  {name: 'Surrey',
+  annual_revenue: '224000',
+  mens_apparel: false,
+  womens_apparel: true},
+  {name: 'Whistler',
+  annual_revenue: '1900000',
+  mens_apparel: true,
+  womens_apparel: false},
+  {name: 'Yaletown',
+  annual_revenue: '430000',
+  mens_apparel: true,
+  womens_apparel: true}
+])
+
+@mens_stores = Store.where(mens_apparel: true).find_each do |store|
+  puts store.name
+end
+
+Store.where("womens_apparel = true AND annual_revenue < '1000000'").find_each do |store|
+  puts store.name
+end
